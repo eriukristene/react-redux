bfb142abe28ac8277206763978aa4bd327233726
eriukristene <22382697+eriukristene@users.noreply.github.com>
2020-09-14 12:02:41 +0200

    Add files via upload

---
+++ redux-burger--01-basic-setup/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
+
+const app = (
+    // connect our store, created by redux, with our react application
+    // by using a wrapper component Provider
+    // all props passed down to the wrapping application
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+);
+
+ReactDOM.render( app, document.getElementById( 'root' ) );
+registerServiceWorker();
---
+++ redux-burger--01-basic-setup/src/store/reducer.js
@@ -0,0 +1,15 @@
+import * as actionTypes from './actions';
+
+// this is what our state should look like initially
+// want to use the same state that we use in the BurgerBuilder
+const initialState = {
+    ingredients: null,
+    totalPrice: 4
+};
+
+// get the state, and set it to initialState
+const reducer = (state = initialState, action) => {
+
+};
+
+export default reducer;
